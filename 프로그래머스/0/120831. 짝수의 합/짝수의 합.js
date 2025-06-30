7d78c468e1fb8933e9c255fd329094dcdc269450
Rimi <whi1230@naver.com>
2025-06-30 22:48:57 +0900

    [level 0] Title: 짝수의 합, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.js
@@ -0,0 +1,11 @@
+const solution = (n) => {
+    let answer = 0;
+    for (let i = n; i > 0; i--) {
+        if (i % 2 === 0) {
+            answer += i;
+        } else {
+            continue;
+        }
+    }
+    return answer;
+}
